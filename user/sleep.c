4f694e94d019dca8065c5a69e305974bc794314e
Alian-yy <3279932900@qq.com>
2025-07-11 17:51:52 +0800

    first

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: sleep <ticks>\n");
+        exit(1);
+    }
+
+    int ticks = atoi(argv[1]);
+    if (ticks < 0) {
+        printf("Error: ticks must be a positive integer\n");
+        exit(1);
+    }
+
+    sleep(ticks);
+    printf("(nothing happens for a little while)\n");
+    exit(0);
+}
